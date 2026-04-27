e9d355cf699a9d27e1738f1500f049bb5d7be2bc
idk-codingal <mokshnagda2020@gmail.com>
2026-04-27 16:46:15 -0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,3 @@
+list = ['potato', 'pancake']
+dictionary = {"Name" : "Moksh" , "Age" : 11}
+print(dictionary)
---
+++ main4.py
@@ -0,0 +1,22 @@
+my_tup = ()
+print(my_tup)
+
+my_tup2 = (13,452,356,47)
+print(my_tup2)
+
+my_tup3 = (1,5,7,'hi',True,4,'jjk', 1.534634635755785468512345654345654345434543454345434545454)
+print(my_tup3)
+
+my_tup4 = (1,2,'Fortnite', 'Minecraft', ['this is a list', 3, 4, 68, 346])
+print(my_tup4)
+
+my_tup4 = (1,2,'Fortnite', 'Minecraft', ['this is a list', 3, 4, 68, 346])
+print(my_tup4[3])
+print("sliced", (my_tup4[1:3]))
+
+#Creating Sets
+set = {1,2,4,5,2,5}
+print(set)
+
+my_set = {}
+print(my_set)
